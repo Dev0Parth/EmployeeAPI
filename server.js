c543c8136eab163b4ec19cbea992d44a6b156f45
Parth <parth.patel00@outlook.com>
2023-03-02 23:28:31 +0530

    start conn in starting & Create views directory

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ const connection = mysql.createConnection({
   database: 'ygm'
 });
 
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+    res.sendStatus(500);
+    return;
+  }
+});
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,30 +34,15 @@ app.get("/", (req, res) => {
 
 app.get('/home', (req, res) => {
 
-  connection.connect((err) => {
-    if (err) {
-      res.send("Connection Error!");
-      return;
-    }
-  });
-
   const query = `SELECT * FROM employee`;
 
   connection.query(query, (err, results) => {
     if (err) {
       res.send("Query Error!");
-    }
-
-    res.render('home', { data: results });
-  });
-
-  connection.end((err) => {
-    if (err) {
-      res.send("Connection Closing Error!");
       return;
     }
 
-    res.send("Connection Closed!");
+    res.render('home', { data: results });
   });
 
 });
@@ -58,35 +51,20 @@ app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-
-  connection.connect((err) => {
-    if (err) {
-      res.send("Connection Error!");
-      return;
-    }
-  });
-
   const query = `SELECT * FROM admins WHERE username = ? AND password = ?`;
   const values = [username, password];
 
   connection.query(query, values, (err, results) => {
     if (err) {
       res.send("Query Error!");
+      return;
     }
     if (results.length > 0) {
       res.redirect('/home');
     } else {
       res.send("Invalid username or password!");
-    }
-  });
-
-  connection.end((err) => {
-    if (err) {
-      res.send("Connection Closing Error!");
       return;
     }
-
-    res.send("Connection Closed!");
   });
 
 });
@@ -101,14 +79,6 @@ app.post('/submitData', (req, res) => {
   const leaveReason = req.body.leaveReason;
   const createdAt = req.body.createdAt;
 
-  connection.connect((err) => {
-    if (err) {
-      res.send("Database Connection Error!");
-      return;
-    }
-
-    res.send("Database Connected!");
-  });
 
   const sql = `INSERT INTO employee (fullName, presentOrLeave, halfLeave, fullLeave, workDone, leaveReason, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?)`;
   const values = [fullName, presentOrLeave, halfLeave, fullLeave, workDone, leaveReason, createdAt];
@@ -121,16 +91,6 @@ app.post('/submitData', (req, res) => {
     }
 
     res.send("Success!");
-  });
-
-
-  connection.end((err) => {
-    if (err) {
-      res.send("Connection Closing Error!");
-      return;
-    }
-
-    res.send("Connection Closed!");
   });
 
 });
